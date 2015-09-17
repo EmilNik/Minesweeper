5d99937039f51637b5ea3237f386742f5ba7a7e9
Emil Nik <emil.nik@abv.bg>
2015-09-17 19:46:20 +0300

    Refactoring the method PrintScoreBoard in ScoreBoard.cs

--- Minesweeper/ScoreBoard.cs
+++ Minesweeper/ScoreBoard.cs
@@ -29,44 +29,7 @@
 
         public void PrintScoreBoard()
         {
-            //bool firstFive = false;
-            //int currentCounter = 1;
-
-            //Console.WriteLine();
-
-            //if (this.scoreBoard.Values.Count == 0)
-            //{
-            //    Console.WriteLine("Scoreboard empty!");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Scoreboard:");
-
-            //    foreach (int key in this.scoreBoard.Keys.OrderByDescending(obj => obj))
-            //    {
-            //        foreach (string person in this.scoreBoard[key])
-            //        {
-            //            // TODO
-            //            if (currentCounter < 6)
-            //            {
-            //                Console.WriteLine("{0}. {1} --> {2} cells", currentCounter, person, key);
-            //                currentCounter++;
-            //            }
-            //            else
-            //            {
-            //                firstFive = true;
-            //                break;
-            //            }
-            //        }
-
-            //        if (firstFive)
-            //        {
-            //            break;
-            //        }
-            //    }
-
-                var numberOfPrintedNames = 5;
-
+            var numberOfPrintedNames = 5;
             var keys = this.scoreBoard.Keys.OrderByDescending(obj => obj).ToArray();
 
             if (keys.Length < numberOfPrintedNames)
