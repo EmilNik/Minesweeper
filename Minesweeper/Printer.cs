2e26ef7eecbe903078a0bbaec2f15373a62cf8cf
NikolaiMishev <nikolaimishev@gmail.com>
2015-09-26 18:08:16 +0300

    Get rid of 'go to'. Moved PrintInitialMessage and PrintField in Printer class.

--- Minesweeper/Printer.cs
+++ Minesweeper/Printer.cs
@@ -9,7 +9,45 @@ namespace Minesweeper
     {
         public void PrintScoreBoard(IScoreBoard scoreBoard)
         {
+        }
 
+        public void PrintField(string[,] minesMatrix, bool boomed)
+        {
+            Console.WriteLine();
+            Console.WriteLine("     0 1 2 3 4 5 6 7 8 9");
+            Console.WriteLine("   ---------------------");
+            for (int i = 0; i < minesMatrix.GetLength(0); i++)
+            {
+                Console.Write("{0} | ", i);
+                for (int j = 0; j < minesMatrix.GetLength(1); j++)
+                {
+                    if (!(boomed) && ((minesMatrix[i, j] == "") || (minesMatrix[i, j] == "*")))
+                    {
+                        Console.Write(" ?");
+                    }
+                    if (!(boomed) && (minesMatrix[i, j] != "") && (minesMatrix[i, j] != "*"))
+                    {
+                        Console.Write(" {0}", minesMatrix[i, j]);
+                    }
+                    if ((boomed) && (minesMatrix[i, j] == ""))
+                    {
+                        Console.Write(" -");
+                    }
+                    if ((boomed) && (minesMatrix[i, j] != ""))
+                    {
+                        Console.Write(" {0}", minesMatrix[i, j]);
+                    }
+
+                }
+                Console.WriteLine("|");
+            }
+            Console.WriteLine("   ---------------------");
+        }
+
+        public void PrintInitialMessage()
+        {
+            string startMessage = @"Welcome to the game “Minesweeper”. Try to reveal all cells without mines. Use   'top' to view the scoreboard, 'restart' to start a new game and 'exit' to quit  the game.";
+            Console.WriteLine(startMessage + "\n");
         }
     }
 }
