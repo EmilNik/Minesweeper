49da6def36444a5025355cdf87d90bd4be28fcee
Stelamat <Steliyana.Mateeva@gmail.com>
2015-10-06 02:01:39 +0300

    Revert "Added New Test Method"
    This reverts commit b522a39f12408b9944b04400865b2ab832664b53.

--- Minesweeper/Printer.Tests/PrintInitialMessageTest.cs
+++ Minesweeper/Printer.Tests/PrintInitialMessageTest.cs
@@ -18,5 +18,28 @@ namespace Printer.Tests
            // var result = printer.PrintMessage(message);
             //Assert.AreEqual(true, result);
         }*/
+
+        [TestMethod]
+        public void TestGetDefaultFieldMethod()
+        {
+            int row = 5;
+            int call = 10;
+            int mines = 15;
+            string[,] testMineFiled = new string[row, call];
+            string emptySymbol = "";
+            IField mineFieldTest = new Minesweeper.Field(row, call, mines);
+            mineFieldTest.GetDefaultField();
+
+        
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < call; j++)
+                {
+                    testMineFiled[i, j] = "";
+                    Assert.AreEqual(emptySymbol, testMineFiled[i, j]);
+                }
+            }
+        }
+
     }
 }
