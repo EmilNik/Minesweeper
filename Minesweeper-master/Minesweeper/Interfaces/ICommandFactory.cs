16f98096d0a698b6b2cd500a7850139c193e3e4f
Stelamat <Steliyana.Mateeva@gmail.com>
2015-10-06 01:34:54 +0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Minesweeper/Minichki.cs
    	Minesweeper/Printer.cs

---
+++ Minesweeper-master/Minesweeper/Interfaces/ICommandFactory.cs
@@ -0,0 +1,13 @@
+﻿using Minesweeper.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Minesweeper
+{
+    public interface ICommandFactory
+    {
+        ICommand CreateCommand(string command);
+    }
+}
---
+++ Minesweeper/Printer.Tests/PrintInitialMessageTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Minesweeper;
+
+namespace Printer.Tests
+
+{
+    [TestClass]
+    public class PrintInitialMessageTest
+    {
+        /*
+        private IField Minesweeper;
+        [TestMethod]
+        public void PrintMessageTest()
+        {
+            IPrinter printer = new Minesweeper.Printer();
+            string message = "This is a TEST PRINT";
+           // var result = printer.PrintMessage(message);
+            //Assert.AreEqual(true, result);
+        }*/
+
+        [TestMethod]
+        public void TestGetDefaultFieldMethod()
+        {
+            int row = 5;
+            int call = 10;
+            int mines = 15;
+            string[,] testMineFiled = new string[row, call];
+            string emptySymbol = "";
+            IField mineFieldTest = new Minesweeper.Field(row, call, mines);
+            mineFieldTest.GetDefaultField();
+
+        
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < call; j++)
+                {
+                    testMineFiled[i, j] = "";
+                    Assert.AreEqual(emptySymbol, testMineFiled[i, j]);
+                }
+            }
+        }
+
+    }
+}
